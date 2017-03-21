1d8f9e4073d37425fa3d12f3ad572399d00d72f1
Shiva Sitamraju <shiva@blockonomics.co>
2017-03-21 11:29:43 +0530

    Fixed url to wallet watcher

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -332,7 +332,7 @@ function show_options()
     <form method="post" action="options.php">
     <?php wp_nonce_field('update-options') ?>
   <table class="form-table">
-    <tr valign="top"><th scope="row">BLOCKONOMICS API KEY (Generate from <a href="https://blockonomics.co/blockonomics">Wallet Watcher</a> &gt; Settings)</th>
+    <tr valign="top"><th scope="row">BLOCKONOMICS API KEY (Generate from <a href="https://www.blockonomics.co/blockonomics">Wallet Watcher</a> &gt; Settings)</th>
     <td><input type="text" name="blockonomics_api_key" value="<?php echo get_option('blockonomics_api_key'); ?>" /></td>
     </tr>
     <tr valign="top"><th scope="row">CALLBACK URL (Copy this url and set in <a href="https://www.blockonomics.co/merchants">Merchants</a>)</th>
