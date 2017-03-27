c2cccf509b1d7b9319fd39f3861f591be6b9e153
Shiva Sitamraju <shiva@blockonomics.co>
2017-03-27 15:08:53 +0530

    Increasing version to 1.2

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Blockonomics Bitcoin Payments
  * Plugin URI: https://github.com/blockonomics/blockonomics-woocommerce
  * Description: Accept Bitcoin on your WooCommerce-powered website with Blockonomics
- * Version: 1.0.3
+ * Version: 1.2
  * Author: Blockonomics
  * Author URI: https://www.blockonomics.co
  * License: MIT
