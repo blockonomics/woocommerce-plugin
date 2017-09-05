88b65f2e2a47c68f9b0a9be1c7e375089709ee7e
Shiva Sitamraju <shiva@blockonomics.co>
2017-09-05 21:48:28 +0530

    Added extra custom fields

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Blockonomics Bitcoin Payments
  * Plugin URI: https://github.com/blockonomics/woocommerce-plugin
  * Description: Accept Bitcoin on your WooCommerce-powered website with Blockonomics
- * Version: 1.3.6
+ * Version: 1.3.7
  * Author: Blockonomics
  * Author URI: https://www.blockonomics.co
  * License: MIT
