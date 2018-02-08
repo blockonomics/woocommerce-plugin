0f503cbf4e1b9662b7088447c42ffad32a9fb151
Shiva Sitamraju <shiva@blockonomics.co>
2018-02-08 12:13:47 +0530

    see #49. Added payment details sections with help text

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -324,7 +324,7 @@ if (is_plugin_active('woocommerce/woocommerce.php') || class_exists('WooCommerce
             $address = get_post_meta($order->id, 'blockonomics_address', true);
             include_once plugin_dir_path(__FILE__) . 'php' . DIRECTORY_SEPARATOR . 'Blockonomics.php';
             if ($txid && $address) {
-                echo '<p><strong>'.__('Transaction').':</strong>  <a href =\''. Blockonomics::BASE_URL ."/api/tx?txid=$txid&addr=$address'>".substr($txid, 0, 10). '</a></p>';
+                echo '<h2>Payment Details</h2><p><strong>'.__('Transaction').':</strong>  <a href =\''. Blockonomics::BASE_URL ."/api/tx?txid=$txid&addr=$address'>".substr($txid, 0, 10). '</a></p><p>Your order will be processed on confirmation of above transaction by the bitcoin network.</p>';
             }
         }
 
