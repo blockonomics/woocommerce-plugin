cba7718ee7488c7a9741a1fa0ac4b90f34abe28b
DarrenWestwood <darrenwestwood86@gmail.com>
2019-01-07 15:34:20 +0200

    Custom timeout for cURL

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -780,4 +780,22 @@ function bnomics_email_woocommerce_style($email, $subject, $heading, $message) {
   $mailer->send( $email, $subject, $html_message, array('Content-Type: text/html; charset=UTF-8') );
 }
 
+// Setting a custom timeout value for cURL. Using a high value for priority to ensure the function runs after any other added to the same action hook.
+add_action('http_api_curl', 'bnomics_custom_curl_timeout', 9999, 1);
+function bnomics_custom_curl_timeout( $handle ){
+    curl_setopt( $handle, CURLOPT_CONNECTTIMEOUT, 30 ); // 30 seconds. Too much for production, only for testing.
+    curl_setopt( $handle, CURLOPT_TIMEOUT, 30 ); // 30 seconds. Too much for production, only for testing.
+}
+// Setting custom timeout for the HTTP request
+add_filter( 'http_request_timeout', 'bnomics_custom_http_request_timeout', 9999 );
+function bnomics_custom_http_request_timeout( $timeout_value ) {
+    return 30; // 30 seconds. Too much for production, only for testing.
+}
+// Setting custom timeout in HTTP request args
+add_filter('http_request_args', 'bnomics_custom_http_request_args', 9999, 1);
+function bnomics_custom_http_request_args( $r ){
+    $r['timeout'] = 30; // 30 seconds. Too much for production, only for testing.
+    return $r;
+}
+
 ?>
