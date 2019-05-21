6bde5b0e46e5a77a6a7ffbe22754cb48e548396b
DarrenWestwood <darrenwestwood86@yahoo.com>
2019-05-21 02:13:48 +0200

    Update issue #124, only increase new_address timeout

--- php/Blockonomics.php
+++ php/Blockonomics.php
@@ -40,7 +40,7 @@ class Blockonomics
             $get_params = "?match_callback=$secret";
         }
         $url = Blockonomics::NEW_ADDRESS_URL.$get_params;
-        $response = $this->post($url, $this->api_key);
+        $response = $this->post($url, $this->api_key, '', 8);
         if (!isset($responseObj)) $responseObj = new stdClass();
         $responseObj->{'response_code'} = wp_remote_retrieve_response_code($response);
         if (wp_remote_retrieve_body($response))
@@ -129,13 +129,13 @@ class Blockonomics
         }
     }
 
-    private function post($url, $api_key = '', $body = '', $type = '')
+    private function post($url, $api_key = '', $body = '', $timeout = 5)
     {
         $headers = $this->set_headers($api_key);
 
         $response = wp_remote_post( $url, array(
             'method' => 'POST',
-            'timeout' => 8,
+            'timeout' => $timeout,
             'headers' => $headers,
             'body' => $body
             )
