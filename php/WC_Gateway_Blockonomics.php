1f6c45c070f2f6afe4eae37cc86bbe8bcee17cf7
Darren <darrenwestwood86@gmail.com>
2019-12-11 15:36:12 +0200

    Rename order.php to blockonomics_checkout.php

--- php/WC_Gateway_Blockonomics.php
+++ php/WC_Gateway_Blockonomics.php
@@ -154,8 +154,8 @@ class WC_Gateway_Blockonomics extends WC_Payment_Gateway
     public function redirect_to_template($template){
         add_action('wp_enqueue_scripts', 'bnomics_enqueue_stylesheets' );
         add_action('wp_enqueue_scripts', 'bnomics_enqueue_scripts' );
-        if($template == 'order.php'){
-            $theme_template = 'blockonomics_checkout.php';
+        if($template == 'blockonomics_checkout.php'){
+            $theme_template = $template;
         }
         if ( $overridden_template = locate_template( $theme_template ) ) {
             // locate_template() returns path to file
@@ -175,7 +175,7 @@ class WC_Gateway_Blockonomics extends WC_Payment_Gateway
         $address = isset($_REQUEST["show_order"]) ? $_REQUEST["show_order"] : "";
         $uuid = isset($_REQUEST["uuid"]) ? $_REQUEST["uuid"] : "";
         if ($address) {
-            $this->redirect_to_template('order.php');
+            $this->redirect_to_template('blockonomics_checkout.php');
         }else if ($uuid){
             $this->redirect_to_template('track.php');
         }
