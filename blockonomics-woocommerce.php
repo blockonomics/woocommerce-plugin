74433279cb4e0e2611bba14ceefac6aa6aa9965b
Christopher Nohall <christopher.nohall@gmail.com>
2021-01-14 16:47:25 +0900

    renamed functions to clearer names

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -58,7 +58,7 @@ function blockonomics_woocommerce_init()
     add_action('init', 'load_plugin_translations');
     add_action('woocommerce_order_details_after_order_table', 'nolo_custom_field_display_cust_order_meta', 10, 1);
     add_action('woocommerce_email_customer_details', 'nolo_bnomics_woocommerce_email_customer_details', 10, 1);
-    add_action('admin_menu', 'bitcoin_address_finder');
+    add_action('admin_menu', 'find_bitcoin_order');
     add_filter('woocommerce_payment_gateways', 'woocommerce_add_blockonomics_gateway');
     add_filter('clean_url', 'bnomics_async_scripts', 11, 1 );
 
@@ -401,8 +401,8 @@ register_activation_hook( __FILE__, 'blockonomics_activation_hook' );
 add_action('admin_notices', 'blockonomics_plugin_activation');
 
 
-function bitcoin_address_finder(){
-    add_menu_page( 'Bitcoin Address Finder', 'Bitcoin Address Finder', 'manage_options', 'bitcoin-address-finder', 'find_woocommerce_order' );
+function find_bitcoin_order(){
+    add_menu_page( 'Find Order based on Address', 'Find Bitcoin Order', 'manage_options', 'bitcoin-order-finder', 'find_woocommerce_order' );
 }
 
 function find_woocommerce_order(){
