4f8c4c098100ac45175ba4cefe8ded50deb134e1
Christopher Nohall <christopher.nohall@gmail.com>
2021-01-25 21:52:26 +0900

    type "button" disappeared when merging with master

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -346,7 +346,7 @@ function blockonomics_woocommerce_init()
                     <input type="submit" class="button-primary" value="Save"/>
                     <input type="hidden" name="action" value="update" />
                     <input type="hidden" name="page_options" value="blockonomics_api_key,blockonomics_bch,blockonomics_timeperiod,blockonomics_margin,blockonomics_gen_callback,blockonomics_api_updated,blockonomics_underpayment_slack,blockonomics_lite,blockonomics_nojs,blockonomics_network_confirmation" />
-                    <input onclick="checkForAPIKeyChange();" class="button-primary" name="test-setup-submit" value="Test Setup" style="max-width:85px;">
+                    <input type="button" onclick="checkForAPIKeyChange();" class="button-primary" name="test-setup-submit" value="Test Setup" style="max-width:85px;">
                 </p>
             </form>
             <form method="POST" name="testSetupForm">
