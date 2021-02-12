ff157408a02de2d990124b57d705799cb7c03ed7
Christopher Nohall <christopher.nohall@gmail.com>
2021-02-12 20:15:02 +0900

    should now work with address/txid in one field

--- php/WC_Filter.php
+++ php/WC_Filter.php
@@ -7,39 +7,23 @@ class WC_Filter_Orders_By_Address {
 	public function __construct() {
 		if ( is_admin() ) {
 			// add bulk order filter for exported / non-exported orders
-			add_action( 'restrict_manage_posts', array( $this, 'filter_orders_by_address') , 20 );
-			add_filter( 'request',               array( $this, 'filter_orders_by_query' ) );	
+			add_action( 'restrict_manage_posts', array( $this, 'filter_orders') , 20 );
+			add_filter( 'request',               array( $this, 'filter_orders_by_address_or_txid' ) );	
+
 		}
 	}
-	public function filter_orders_by_address() {
+	public function filter_orders() {
 		global $typenow;
-
-		// add_settings_error('option_notice', 'option_notice', 'Hello', 'error');
 		if ( 'shop_order' === $typenow ) {
 			?>
-			<input type='name' placeholder='Filter bitcoin payments' name='filter_by_address'>
-			<select name="type_query" id="dropdown_type_query">
-				<option value="btc_address">
-					<?php esc_html_e( 'Bitcoin Address' ); ?>
-				</option>
-				<option value="bch_address">
-					<?php esc_html_e( 'Bitcoin Cash Address' ); ?>
-				</option>
-				<option value="blockonomics_btc_txid">
-					<?php esc_html_e( 'Bitcoin txid' ); ?>
-				</option>
-				<option value="blockonomics_bch_txid">
-					<?php esc_html_e( 'Bitcoin Cash txid' ); ?>
-				</option>
-			</select>
+			<input size='26' type='name' placeholder='Filter by crypto address/txid' name='filter_by'>
 			<?php
 		}
 	}
-	public function filter_orders_by_query( $vars ) {
+	public function filter_orders_by_address_or_txid( $vars ) {
 		global $typenow;
-		if ( 'shop_order' === $typenow && isset( $_GET['filter_by_address'] ) && ! empty( $_GET['filter_by_address'] && isset( $_GET['type_query'] ) && ! empty( $_GET['type_query']))){
-			$vars['meta_key']   = $_GET['type_query'];
-			$vars['meta_value'] = wc_clean( $_GET['filter_by_address'] );
+		if ( 'shop_order' === $typenow && isset( $_GET['filter_by'] ) && ! empty( $_GET['filter_by'])){
+			$vars['meta_value'] = wc_clean( $_GET['filter_by'] );
 		}
 		return $vars;
 	}
