c011eff8ccb27b3bb5985d5de05301b667419044
cnohall <christopher.nohall@gmail.com>
2021-04-09 17:45:00 +0900

    using previous logic, only changed "create table"

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -530,17 +530,20 @@ function blockonomics_update_db_check() {
     global $blockonomics_db_version;
 
     $installed_ver = get_site_option( 'blockonomics_db_version' );
-    if ( $installed_ver != $blockonomics_db_version ) {
+    if (!$installed_ver) {
+        blockonomics_create_table();
+    }else if ( $installed_ver != $blockonomics_db_version ) {
+
         // Example function to demonstrate table changes between upgrade versions
         // if ($installed_ver < 1.0) {
         //     $wpdb->query("ALTER TABLE $table_name DROP transaction;");
         // }
-        blockonomics_create_table();
-    }
-}
 
+        update_option( 'blockonomics_db_version', $blockonomics_db_version );
+    }
+    
+}
 add_action( 'plugins_loaded', 'blockonomics_update_db_check' );
-register_activation_hook( __FILE__, 'blockonomics_create_table' );
 
 //Show message when plugin is activated
 function blockonomics_plugin_activation() {
