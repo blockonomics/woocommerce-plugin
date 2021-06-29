980a25f15a1defe7934e4f51be4bd00b9f2e9f0a
cnohall <christopher.nohall@gmail.com>
2021-06-29 21:46:26 +0900

    change <a> to onclick=windows.location

--- templates/blockonomics_crypto_options.php
+++ templates/blockonomics_crypto_options.php
@@ -12,17 +12,15 @@ $order_url = $blockonomics->get_parameterized_wc_url(array('show_order'=>$order_
         foreach ($cryptos as $code => $crypto) {
           $order_url = add_query_arg('crypto', $code, $order_url);
         ?>
-          <td class="bnomics-select-options">
-            <a href="<?php echo $order_url;?>" style="color: inherit; text-decoration: inherit;">
-              <p>
-                <?=__('Pay With', 'blockonomics-bitcoin-payments')?>
-              </p>
-              <span class="bnomics-icon-<?php echo $code;?> bnomics-rotate-<?php echo $code;?>"></span>
-              <p>
-                <?php echo $crypto['name'];?><br>
-                <b><?php echo $code;?></b>
-              </p>
-            </a>
+          <td onclick="window.location='<?php echo $order_url;?>'" class="bnomics-select-options">
+            <p>
+              <?=__('Pay With', 'blockonomics-bitcoin-payments')?>
+            </p>
+            <span class="bnomics-icon-<?php echo $code;?> bnomics-rotate-<?php echo $code;?>"></span>
+            <p>
+              <?php echo $crypto['name'];?><br>
+              <b><?php echo $code;?></b>
+            </p>
           </td>
         <?php 
         }
