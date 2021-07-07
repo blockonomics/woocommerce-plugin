31a027434905e7694ec49f711f22c0fabc65fce5
cnohall <christopher.nohall@gmail.com>
2021-07-07 15:57:13 +0200

    wip

--- templates/blockonomics_crypto_options.php
+++ templates/blockonomics_crypto_options.php
@@ -4,7 +4,7 @@ $cryptos = $blockonomics->getActiveCurrencies();
 $order_id = $_REQUEST['select_crypto'];
 $order_url = $blockonomics->get_parameterized_wc_url(array('show_order'=>$order_id))
 ?>
-<div class="bnomics-order-container">
+<div class="woocommerce bnomics-order-container">
   <div class="bnomics-select-container">
     <table width="100%">
       <tr>
@@ -12,21 +12,17 @@ $order_url = $blockonomics->get_parameterized_wc_url(array('show_order'=>$order_
         foreach ($cryptos as $code => $crypto) {
           $order_url = add_query_arg('crypto', $code, $order_url);
         ?>
-          <td onclick="window.location='<?php echo $order_url;?>'" class="bnomics-select-options">
-            <p class='hide-on-mobile'>
-              <?=__('Pay With', 'blockonomics-bitcoin-payments')?>
-            </p>
+          <button onclick="window.location='<?php echo $order_url;?>'" class="bnomics-select-options woocommerce-button button">
             <span class="bnomics-icon-<?php echo $code;?> bnomics-rotate-<?php echo $code;?>"></span>
-            <b class='show-on-mobile'><?php echo $crypto['name'];?></b>
-            
-            <p class='hide-on-mobile' >
-              <?php echo $crypto['name'];?><br>
-              <b><?php echo $code;?></b>
-            </p>
-          </td>
+            <span class="vertical-line">
+              <?=__('Pay With', 'blockonomics-bitcoin-payments')?>
+              <?php echo $crypto['name'];?>
+            </span>
+        </button>
         <?php 
         }
         ?>
+
       </tr>
     </table>
   </div>
