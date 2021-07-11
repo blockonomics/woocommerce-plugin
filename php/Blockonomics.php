7d3483b3ec058378eef43291eea8fb2be94b4ffe
cnohall <christopher.nohall@gmail.com>
2021-07-11 13:13:02 +0200

    reinstated timestamp in previous position

--- php/Blockonomics.php
+++ php/Blockonomics.php
@@ -466,8 +466,8 @@ class Blockonomics
                 $price = 1;
             }
             $order['satoshi'] = intval(round(1.0e8*$wc_order->get_total()/$price));
+            $order['timestamp'] = time();
         }
-        $order['timestamp'] = isset($order['timestamp']) ? $order['timestamp'] : time();
         return $order;
     }
 
