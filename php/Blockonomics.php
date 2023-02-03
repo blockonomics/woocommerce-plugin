94d24815a6fe731f250076743f4330214e88d068
ankit61d <ankit61d@gmail.com>
2023-02-03 12:04:17 +0530

    send email on underpayment

--- php/Blockonomics.php
+++ php/Blockonomics.php
@@ -844,6 +844,7 @@ class Blockonomics
         $order['paid_fiat'] =number_format($order['expected_fiat']*$paid_amount_ratio,2,'.','');
         if ($this->is_order_underpaid($order)) {
             $this->add_coupon_on_underpayment($value, $order, $wc_order);
+            $this->send_email_on_underpayment($order);
             $wc_order->save;
         }
         else{
@@ -910,6 +911,15 @@ class Blockonomics
 
         $coupon_note = "Partial payment received for " .get_woocommerce_currency()." ".sprintf('%0.2f', round($coupon->get_amount(), 2)). " and applied as a coupon.";
         $wc_order->add_order_note(__( $coupon_note, 'blockonomics-bitcoin-payments' ));
+    }
+
+    // Send Invoice email to customer to pay remaining amount
+    public function send_email_on_underpayment($order){
+        $wc_email = WC()->mailer()->emails['WC_Email_Customer_Invoice'];
+        $wc_email->settings['subject'] = __('Additional Payment Required for order #{order_number} on {site_title}');
+        $wc_email->settings['heading'] = __('Use below link to pay remaining amount.'); 
+        $wc_email->settings['additional_content'] = __('<strong>Note: Your existing payment has been applied as a discount to the order</strong>'); 
+        $wc_email->trigger($order['order_id']);
     }
 
     public function generate_qrcode_svg_element($data) {
