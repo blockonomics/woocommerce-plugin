59760ee91aa489d27b52c79f92ae8efaa80f07e3
Aishwarya adyanthaya <91294460+aadyanthaya@users.noreply.github.com>
2023-04-25 18:28:28 +0100

    Disabled the partial payement

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -332,7 +332,7 @@ function blockonomics_woocommerce_init()
                             </tr>
                             <tr valign="top">
                                 <th scope="row"><?php echo __('Allow Partial Payments (Customer can pay order via multiple payments)', 'blockonomics-bitcoin-payments')?></th>
-                                <td><input onchange="add_asterisk('settings')" type="checkbox" name="blockonomics_partial_payments" value="1" <?php checked("1", get_option('blockonomics_partial_payments', $default_value = true)); ?> /></td>
+                                <td><input onchange="add_asterisk('settings')" type="checkbox" name="blockonomics_partial_payments" value="1" <?php checked("1", get_option('blockonomics_partial_payments', $default_value = false)); ?> /></td>
                             </tr>
                         </table>
                     </div>
