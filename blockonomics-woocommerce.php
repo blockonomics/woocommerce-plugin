20fa4549d7a8a9d73e159fe5be00823daa73a0d6
anktd <ankit61d@gmail.com>
2024-12-04 12:47:16 +0530

    fix: Remove incorrect wpdb::prepare usage in uninstall hook

--- blockonomics-woocommerce.php
+++ blockonomics-woocommerce.php
@@ -538,7 +538,9 @@ function blockonomics_uninstall_hook() {
     global $wpdb;
     // drop blockonomics_orders & blockonomics_payments on uninstallation
     // blockonomics_orders was the payments table before db version 1.2
-    $wpdb->query($wpdb->prepare("DROP TABLE IF EXISTS ".$wpdb->prefix."blockonomics_orders , ".$wpdb->prefix."blockonomics_payments"));
+    // Fix: Add proper placeholder in the query
+    $wpdb->query("DROP TABLE IF EXISTS ".$wpdb->prefix."blockonomics_orders");
+    $wpdb->query("DROP TABLE IF EXISTS ".$wpdb->prefix."blockonomics_payments");
     delete_option("blockonomics_db_version");
 
     // Remove the custom page and shortcode added for payment
